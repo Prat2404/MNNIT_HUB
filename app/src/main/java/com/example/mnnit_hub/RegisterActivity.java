71721ccdbb22ba8a11a82b766888424759dc2332
Prat2404 <prateekg.gupta2001@gmail.com>
2021-02-22 00:13:43 +0530

    Login added

--- app/src/main/java/com/example/mnnit_hub/RegisterActivity.java
+++ app/src/main/java/com/example/mnnit_hub/RegisterActivity.java
@@ -3,12 +3,19 @@ package com.example.mnnit_hub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class RegisterActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
     }
+
+
 }
