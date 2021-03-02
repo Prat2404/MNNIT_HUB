33281d3150c40196aba35050375f1a2c4e0efe8f
abhikeshri10 <60318491+abhikeshri10@users.noreply.github.com>
2021-03-02 15:32:38 +0530

    Added dashboard activity

--- app/src/main/java/com/example/mnnit_hub/LoginActivity.java
+++ app/src/main/java/com/example/mnnit_hub/LoginActivity.java
@@ -1,4 +1,14 @@
 package com.example.mnnit_hub;
 
-public class LoginActivity {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
 }
